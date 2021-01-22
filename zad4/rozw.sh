db756dba16ed561770169a1fd877f56a3fc045ec
Alflermilosz <s96518@pollub.edu.pl>
2021-01-22 12:00:50 +0000

    Rozwiazanie

--- zad4/rozw.sh
+++ zad4/rozw.sh
@@ -2,8 +2,7 @@
 
 ADRES=212.182.16.243:222
 
-echo ${ADRES%:*}
-
-echo ${ADRES#*:}
-
-wc -m ADRES
+echo $ADRES | cut -d ':' -f 1
+echo $ADRES | cut -d ':' -f 2
+echo "Liczba znakow:"
+echo ${#ADRES}
